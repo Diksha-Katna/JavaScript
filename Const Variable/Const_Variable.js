e2eed8731be3ae3604ff4a973966607fbc57940b
Diksha-Katna <dikshakatnagenuine@gmail.com>
2024-12-06 14:27:21 +0530

    Const Variable

---
+++ Const Variable/Const_Variable.js
@@ -0,0 +1,22 @@
+const age ='40';
+console.log(age);
+
+{
+    const salary=16000
+    const salary= 18000
+}
+console.log(salary); //const variable can not be redeclared
+
+
+{
+    const id=45;
+    id=50;
+    console.log(id);
+    }  // We can not update the const variable
+
+
+    {
+        const roll_number=50;
+
+    }
+    console.log(roll_number); // We can not access const variable outside the block
