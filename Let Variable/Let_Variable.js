3e0ecd76b74d4fddb2cf976eba00e1d2f1056745
Diksha-Katna <dikshakatnagenuine@gmail.com>
2024-12-06 14:26:51 +0530

    Let Variable

--- Let Variable/Let_Variable.js
+++ Let Variable/Let_Variable.js
@@ -1,12 +1,12 @@
 let age=13;
 age=14;
-console.log(age)
+console.log(age);
 
 
 
 let salary= 30000;
 let salary=60000;
-console.log(salary)
+console.log(salary);
 
  //This redeclartion will give error 
 
@@ -14,10 +14,10 @@ console.log(salary)
  {
     let name="Diksha"
  }
- console.log(name)   // This will not get exexuted as this is not accessible outside of the block
+ console.log(name);  // This will not get exexuted as this is not accessible outside of the block
 
  {
     let roll_number=15;
-    console.log(roll_number)
+    console.log(roll_number);
  }
 
--- Var Variable/Var_Variable.js
+++ Var Variable/Var_Variable.js
@@ -1,17 +1,17 @@
 var age=13;
 var age=14;
-console.log(age)
+console.log(age);
 
 
 {
     var salary=15000
 }
 
-console.log(salary)
+console.log(salary);
 
 {
     var id=30
     var id=35
 }
 
-console.log(id)
+console.log(id);
