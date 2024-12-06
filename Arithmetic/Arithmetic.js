2b0ca8447a40a50df7c980a53d087fb7ab48784a
Diksha-Katna <dikshakatnagenuine@gmail.com>
2024-12-06 16:59:09 +0530

    Arithmetic

---
+++ Arithmetic/Arithmetic.js
@@ -0,0 +1,52 @@
+{
+let x=500; 
+let y=200; 
+let sum=x+y;
+let subtraction=y-x;
+let multiply=x*y;
+let divide=x/y;
+let remainder=x%y;
+console.log(sum);
+console.log(subtraction);
+console.log(multiply);
+console.log(divide);
+console.log(remainder);
+
+
+}
+
+{
+let a=100;
+a--;
+let b=a;
+console.log(b);
+}
+
+
+{
+let a=900;
+a++;
+let b=a;
+console.log(b);
+}
+
+{
+let x=5;
+let y=x**2;
+console.log(y);
+
+
+}
+
+{
+    let a=1100*50+1300/2;
+    console.log(a);
+
+}
+
+{
+    let a=10;
+    let b=Math.pow(a,2);
+    console.log(b);
+
+}
