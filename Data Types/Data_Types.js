c34d3b258f387d84d02b767e752fc31c7c012a34
Diksha-Katna <dikshakatnagenuine@gmail.com>
2024-12-06 16:33:48 +0530

    Data Types

---
+++ Data Types/Data_Types.js
@@ -0,0 +1,44 @@
+let x=50; 
+let y=200;                   //Numbers
+console.log(typeof(x));
+console.log(x);
+console.log(typeof(y));
+console.log(y);
+
+let name="John";
+let designation="Engineer"    //String
+console.log(typeof(name));
+console.log(name);
+console.log(typeof(designation));
+console.log(designation);
+
+let exist=true;
+let notexist=false;            //Boolean
+console.log(typeof(exist));
+console.log(exist);
+console.log(typeof(notexist));
+console.log(notexist);
+
+
+const employee={
+    firstname:"John",
+    lastname:"Peter"
+};                                //Object
+
+console.log(typeof(employee));
+console.log(employee);
+
+
+const numeric=["1,2,3,4,5"];       //Array
+console.log(typeof(numeric));
+console.log(numeric);
+
+const date=new Date("2024-12-12");  //Date Object 
+console.log(typeof(date));
+console.log(date);
+
+let a=BigInt("6767865753245566");    //BigInt
+console.log(typeof(a));
+console.log(a);
+
+
