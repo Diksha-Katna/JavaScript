214fb3d075b692f7e17277bacda6c7eaac926966
Diksha-Katna <dikshakatnagenuine@gmail.com>
2024-12-07 15:45:03 +0530

    Assignment Operators

---
+++ Assignment Operators/Assignment_Operators.js
@@ -0,0 +1,31 @@
+let a=5;   //This is assigment operator 
+
+console.log(a);
+
+
+let b =10;
+b+=5;
+ console.log(b);
+
+ let c=12;
+ c-=5;
+ console.log(c);
+
+ let d=8;
+ d*=7;
+ console.log(d);
+
+ let e=10;
+ e/=2;
+ console.log(e);
+
+
+ let f= 10;
+ f**=2;
+ console.log(f);
+
+ let g= 100;
+ g%=50;
+ console.log(g);
+
+ 
