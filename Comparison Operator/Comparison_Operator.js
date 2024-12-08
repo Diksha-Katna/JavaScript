80dac42f44866ae78fc6a7b95e4dcd4243f46947
Diksha-Katna <dikshakatnagenuine@gmail.com>
2024-12-08 12:54:15 +0530

    Comparison_Operators

---
+++ Comparison Operator/Comparison_Operator.js
@@ -0,0 +1,22 @@
+let a=10;
+let b=20;
+let c="10";
+let d=20;
+
+console.log("a==b",a==b);  
+
+console.log("a!=b",a!=b);
+
+console.log("a==c",a==c)  
+
+console.log("a===c",a===c);
+
+console.log("a<b",a<b);
+
+console.log("b<=d", b<=d);
+
+console.log("b>=a",b>=a);
+
+
+
+
