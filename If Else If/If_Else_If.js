6ab7c8eda0c9f8926b9e1a1179787b4728a5b511
Diksha-Katna <dikshakatnagenuine@gmail.com>
2024-12-09 11:31:25 +0530

    If Else If

---
+++ If Else If/If_Else_If.js
@@ -0,0 +1,17 @@
+let x=40;
+if(x<40)
+{
+    console.log("You are not pass ");
+}
+
+else if (x>40)
+{
+    console.log("You have makes above 40 ",x);
+
+}
+
+else
+{
+    console.log("You are Passed");
+    
+}
