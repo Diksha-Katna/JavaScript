8ea3832419db2b589b69a49a3df4c7a2d94d4487
Diksha-Katna <dikshakatnagenuine@gmail.com>
2024-12-11 15:31:25 +0530

    Switch  Statement

---
+++ Switch Statement/Switch_Statement.js
@@ -0,0 +1,20 @@
+let x=2;
+switch(x){
+
+
+    case 0:
+    
+      console.log("zero");
+    
+    case 1:
+    
+       console.log("one");
+
+    case 2:
+
+       console.log("two");
+
+    default:
+
+        console.log("default");
+}
