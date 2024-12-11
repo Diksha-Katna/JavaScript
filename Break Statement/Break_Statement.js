24b641028208be321bb168b79453d6acf73b0a48
Diksha-Katna <dikshakatnagenuine@gmail.com>
2024-12-11 15:58:08 +0530

    Break Statement

---
+++ Break Statement/Break_Statement.js
@@ -0,0 +1,25 @@
+let x=2;
+switch(x){
+
+
+    case 0:
+    
+      console.log("zero");
+      break;
+    
+    case 1:
+    
+       console.log("one");
+       break;
+
+
+    case 2:
+
+       console.log("two");
+       break;
+       
+
+    default:
+
+        console.log("default");
+}
