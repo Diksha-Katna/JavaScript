f8623c5e5f04e48103a534c9ddab562b0d1da017
Diksha-Katna <dikshakatnagenuine@gmail.com>
2024-12-24 16:51:10 +0530

    Arrays

---
+++ Arrays/Arrays.js
@@ -0,0 +1,21 @@
+let marks=[22,44,66,88,99];
+console.log(marks);
+
+console.log(marks[1]);
+
+console.log(marks.length);
+ 
+let i;
+let sum=0;
+
+for(i=1;i<marks.length;i++)
+{
+    sum=sum+marks[i];
+}
+
+console.log(sum);
+
+
+
+
+
