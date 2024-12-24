58cb41d9226f3c9483a2609ff8f919bfc2ece558
Diksha-Katna <dikshakatnagenuine@gmail.com>
2024-12-24 14:51:08 +0530

    Strings

---
+++ Strings/String_Method.js
@@ -0,0 +1,25 @@
+let string1="  Lets Learn  ";
+let string2=" Java Script ";
+
+console.log(string1.length);
+console.log(string1.toUpperCase());
+console.log(string2.toLowerCase());
+console.log(string1.trim());
+
+let result= string1.slice(1,3);
+console.log(result);
+
+
+
+let result1= string1.concat(string2);
+console.log(result1);
+
+let result2= "Hey"+string1+string2;
+console.log(result2);
+
+let result3=string1.replace("Learn", "Study");
+console.log(result3);
+
+
+console.log(string1.charAt(2));
+
